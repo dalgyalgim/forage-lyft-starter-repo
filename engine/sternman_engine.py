52b3ee59b047917069c2efeb0472dc801b7eb80b
Eric Kim <90582033+dalgyalgim@users.noreply.github.com>
2023-02-05 21:50:47 -0800

    Add files via upload

---
+++ engine/sternman_engine.py
@@ -0,0 +1,13 @@
+from engine.engine import Engine
+
+
+class SternmanEngine(Engine):
+    def __init__(self, last_service_date, warning_light_is_on):
+        self.last_service_date = last_service_date
+        self.warning_light_is_on = warning_light_is_on
+
+    def needs_service(self):
+        if self.warning_light_is_on:
+            return True
+        else:
+            return False
